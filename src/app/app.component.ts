99c954e1dd0431579a2bba546f8cee560b608867
ArtemKomarec <artemkomarec10@gmail.com>
2022-02-03 15:07:00 +0300

    feat: add material-design add: basic page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,37 +8,12 @@ import { mergeMap, tap } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  username = 'john';
-  password = '123123123';
   authToken?: string;
   profile?: any;
 
   constructor(
     private readonly httpClient: HttpClient,
   ) {
-  }
-
-  submit() {
-    this.httpClient.post(
-      'http://localhost:3000/auth/login',
-      { username: this.username, password: this.password },
-    ).pipe(
-      tap((response: any) => {
-        if ('access_token' in response) {
-          this.authToken = response.access_token;
-          return;
-        }
-      }),
-      mergeMap(() => {
-        return this.httpClient.get('http://localhost:3000/profile', { headers: { 'Authorization': `Bearer ${this.authToken}` } });
-      }),
-    ).subscribe(
-      (response: any) => {
-        this.profile = response;
-      }, (e) => {
-        console.log('>>', e);
-
-      });
   }
 
   logout() {
